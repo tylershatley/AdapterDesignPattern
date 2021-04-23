6f5e242b061d54ea0e68beac6510ce136c90b0f9
tylershatley <shatleymtyler@gmail.com>
2021-04-23 12:40:52 -0400

    changes

--- Cassett.java
+++ Cassett.java
@@ -1,4 +1,31 @@
 import java.util.ArrayList;
 public class Cassett {
-    public ArrayList
+    private ArrayList<String> songs;
+    private int currentindex;
+
+    public Cassett(String song1, String song2, String song3, 
+    String song4, String song5) {
+        songs = new ArrayList<>();
+        songs.add(song1);
+        songs.add(song2);
+        songs.add(song3);
+        songs.add(song4);
+        songs.add(song5);
+    }
+
+    public String play() {
+        return "";
+    }
+
+    public String rewind() {
+        return "";
+    }
+
+    public String pause() {
+        return "";
+    }
+
+    public String stopEject() {
+        return "";
+    }
 }
