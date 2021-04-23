233099177d8fc415617163b35b22fcc7295eaafe
tylershatley <shatleymtyler@gmail.com>
2021-04-23 12:47:17 -0400

    added CD

---
+++ CD.java
@@ -0,0 +1,38 @@
+public class CD implements DigitalAlbum {
+
+    @Override
+    public String playFromBeginning() {
+        return null;
+    }
+
+    @Override
+    public String playSong(int num) {
+        
+        return null;
+    }
+
+    @Override
+    public String prevSong() {
+        
+        return null;
+    }
+
+    @Override
+    public String nextSong() {
+        
+        return null;
+    }
+
+    @Override
+    public String stop() {
+        
+        return null;
+    }
+
+    @Override
+    public String pause() {
+        
+        return null;
+    }
+    
+}
